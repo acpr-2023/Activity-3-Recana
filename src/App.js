29e40822b8651d7bfa69f1222102abdbef2e9e60
acpr-2023 <144645620+acpr-2023@users.noreply.github.com>
2023-12-22 17:44:08 +0800

    Final code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => setCount(count + 1);
+  const handleDecrement = () => setCount(count - 1); // Allow count to go below 0
+  const handleReset = () => setCount(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className={count >= 0 ? "positive-value" : "negative-value"}>
+        {count}
+      </h1>
+      <button onClick={handleIncrement} className="btn_increase">
+        Increase
+      </button>
+      <button onClick={handleDecrement} className="btn_decrease">
+        Decrease
+      </button>
+      <button onClick={handleReset} className="btn_reset">
+        Reset
+      </button>
     </div>
   );
 }
